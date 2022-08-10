10e7b3cb39772f3f83ff7552f0f5fec9f61c4b18
Benjamin Bini <benjamin.bini@sully-group.fr>
2022-08-10 17:06:04 +0200

    Fix player index

--- src/PlayerHandRanksOdds.js
+++ src/PlayerHandRanksOdds.js
@@ -1,11 +1,13 @@
 import { rankDescription } from "./eval/hand-rank";
 
-const PlayerHandRanksOdds = ({ i, handEval }) => (
+const PlayerHandRanksOdds = ({ playerIndex, handEval }) => (
   <div
-    key={i}
+    key={playerIndex}
     className=" dk:bg-slate-800 dk:shadow-2xl rounded-xl py-2 px-2 text-slate-100"
   >
-    <div className="font-bold mb-2 hidden dk:block">Player {i + 1}</div>
+    <div className="font-bold mb-2 hidden dk:block">
+      Player {playerIndex + 1}
+    </div>
     <ul className="text-xs dk:text-sm font-semibold flex flex-col gap-1 justify-between min-w-[180px]">
       {handEval.handRanks.map((numberOfHands, handRank) => (
         <li key={handRank} className="flex justify-between">
